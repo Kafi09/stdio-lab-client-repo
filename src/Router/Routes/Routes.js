f0e18d3c8a770229d348689f8123a6239e18ba67
Kafi09 <kaficsmy9@gmail.com>
2022-11-10 01:25:07 +0600

    added route change

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -4,6 +4,7 @@ import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
 import CheckOut from "../../Pages/CheckOut/CheckOut";
+import Reviews from "../../Pages/Reviews/Reviews";
 
 const router = createBrowserRouter([
     {
@@ -26,6 +27,10 @@ const router = createBrowserRouter([
                 path: '/checkout/:id',
                 element: <CheckOut></CheckOut>,
                 loader: ({ params }) => fetch(`http://localhost:5000/services/${params.id}`)
+            },
+            {
+                path: '/reviews',
+                element: <Reviews></Reviews>
             }
             
         ]
